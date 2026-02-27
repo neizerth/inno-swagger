c0ee0ff0bd4fe850175976ea933c5b6939dbf74e
Vladimir Yazykov <neizerth@gmail.com>
2026-02-27 21:30:15 +0300

    feat: swagger

--- src/common/constants/env.ts
+++ src/common/constants/env.ts
@@ -1,24 +1,36 @@
-import jetEnv, { num } from 'jet-env';
-import { isValueOf } from 'jet-validators';
-
+import jetEnv, { num } from "jet-env";
 /******************************************************************************
                                  Constants
 ******************************************************************************/
 
 // NOTE: These need to match the names of your ".env" files
 export const NodeEnvs = {
-  DEV: 'development',
-  TEST: 'test',
-  PRODUCTION: 'production',
+	DEV: "development",
+	TEST: "test",
+	PRODUCTION: "production",
 } as const;
+
+/** Returns a jet-env validator that allows only values from the given object. */
+function isValueOf<T extends Record<string, string>>(
+	allowed: T,
+): (arg: unknown, cb?: (v: T[keyof T]) => void) => arg is T[keyof T] {
+	const values = new Set(Object.values(allowed));
+	return (arg: unknown, cb?: (v: T[keyof T]) => void): arg is T[keyof T] => {
+		if (typeof arg === "string" && values.has(arg)) {
+			cb?.(arg as T[keyof T]);
+			return true;
+		}
+		return false;
+	};
+}
 
 /******************************************************************************
                                  Setup
 ******************************************************************************/
 
 const EnvVars = jetEnv({
-  NodeEnv: isValueOf(NodeEnvs),
-  Port: num,
+	NodeEnv: isValueOf(NodeEnvs),
+	Port: num,
 });
 
 /******************************************************************************
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,18 @@
-import express, { NextFunction, Request, Response } from 'express';
-import helmet from 'helmet';
-import logger from 'jet-logger';
-import morgan from 'morgan';
-import path from 'path';
+import express, { NextFunction, Request, Response } from "express";
+import helmet from "helmet";
+import logger from "jet-logger";
+import morgan from "morgan";
+import path from "path";
+import * as fs from "fs";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yaml";
 
-import HttpStatusCodes from '@src/common/constants/HttpStatusCodes';
-import Paths from '@src/common/constants/Paths';
-import { RouteError } from '@src/common/utils/route-errors';
-import BaseRouter from '@src/routes/apiRouter';
+import HttpStatusCodes from "@src/common/constants/HttpStatusCodes";
+import Paths from "@src/common/constants/Paths";
+import { RouteError } from "@src/common/utils/route-errors";
+import BaseRouter from "@src/routes/apiRouter";
 
-import EnvVars, { NodeEnvs } from './common/constants/env';
+import EnvVars, { NodeEnvs } from "./common/constants/env";
 
 /******************************************************************************
                                 Setup
@@ -25,15 +28,15 @@ app.use(express.urlencoded({ extended: true }));
 
 // Show routes called in console during development
 if (EnvVars.NodeEnv === NodeEnvs.DEV) {
-  app.use(morgan('dev'));
+	app.use(morgan("dev"));
 }
 
 // Security
 if (EnvVars.NodeEnv === NodeEnvs.PRODUCTION) {
-  // eslint-disable-next-line no-process-env
-  if (!process.env.DISABLE_HELMET) {
-    app.use(helmet());
-  }
+	// eslint-disable-next-line no-process-env
+	if (!process.env.DISABLE_HELMET) {
+		app.use(helmet());
+	}
 }
 
 // Add APIs, must be after middleware
@@ -41,36 +44,48 @@ app.use(Paths._, BaseRouter);
 
 // Add error handler
 app.use((err: Error, _: Request, res: Response, next: NextFunction) => {
-  if (EnvVars.NodeEnv !== NodeEnvs.TEST.valueOf()) {
-    logger.err(err, true);
-  }
-  let status: HttpStatusCodes = HttpStatusCodes.BAD_REQUEST;
-  if (err instanceof RouteError) {
-    status = err.status;
-    res.status(status).json({ error: err.message });
-  }
-  return next(err);
+	if (EnvVars.NodeEnv !== NodeEnvs.TEST.valueOf()) {
+		logger.err(err, true);
+	}
+	let status: HttpStatusCodes = HttpStatusCodes.BAD_REQUEST;
+	if (err instanceof RouteError) {
+		status = err.status;
+		res.status(status).json({ error: err.message });
+	}
+	return next(err);
 });
 
 // **** FrontEnd Content **** //
 
 // Set views directory (html)
-const viewsDir = path.join(__dirname, 'views');
-app.set('views', viewsDir);
+const viewsDir = path.join(__dirname, "views");
+app.set("views", viewsDir);
 
 // Set static directory (js and css).
-const staticDir = path.join(__dirname, 'public');
+const staticDir = path.join(__dirname, "public");
 app.use(express.static(staticDir));
 
 // Nav to users pg by default
-app.get('/', (_: Request, res: Response) => {
-  return res.redirect('/users');
+app.get("/", (_: Request, res: Response) => {
+	return res.redirect("/users");
 });
 
 // Redirect to login if not logged in.
-app.get('/users', (_: Request, res: Response) => {
-  return res.sendFile('users.html', { root: viewsDir });
+app.get("/users", (_: Request, res: Response) => {
+	return res.sendFile("users.html", { root: viewsDir });
 });
+
+app.get("/ping", (_: Request, res: Response) => {
+	return res.json({
+		message: "pong",
+	});
+});
+
+const swaggerDoc = path.join(__dirname, "docs", "swagger.yaml");
+const file = fs.readFileSync(swaggerDoc, "utf8");
+const swaggerDocument = YAML.parse(file);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 /******************************************************************************
                                 Export default
